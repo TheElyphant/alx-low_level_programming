dd39dc813228359ebcfdc516d4d9440698dcffc2
TheElypant <eodagba@outlook.com>
2023-08-16 00:28:00 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
 	int j;
+	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar(l);
+			_putchar(j);
 		}
 		_putchar(\n);
 	}
