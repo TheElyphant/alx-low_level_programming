0c88755c2aa125eb460c29ef4728038fcf4db430
TheElyphant <eodagba@outlook.com>
2023-08-25 02:50:27 +0100

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,27 @@
 #include "main.h"
 /**
-* *leet - encodes a string into 1337.
-* @n: input value
+* *leet - encodes a string into "1337" on specified replacements.
+* @n: input string.
+*
 * Return: n value
 */
 char *leet(char *n)
 {
-	char s1[] = "aAeEoOtTiL";
-	char s2[] = "4433007711";
+	char leet_chars[] = "aAeEoOtTlL";
+	char leet_replacements[] = "4433007711";
 	int i, j;
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; s1[j] != '\0'; j++)
+		for (j = 0; leet_chars[j] != '\0'; j++)
 		{
-			if (n[i] == s1[j])
+			if (n[i] == leet_chars[j])
 			{
-				n[i] = s2[j];
+				n[i] = leet_replacement[j];
+				break;
 			}
 		}
 	}
+
 	return (n);
 }
