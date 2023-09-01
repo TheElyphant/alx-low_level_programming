95466123f9bd8c8d98578c15ea030c1c7623c416
TheElyphant <eodagba@outlook.com>
2023-09-01 19:42:58 +0100

    create_static_lib.sh

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+
+for file in $c_files; do
+	gcc -c "$file"
+done
+
+ar rcs liball.a *.o
+
+rm -f *.o
+
+echo "Static library liball.a has been created."
