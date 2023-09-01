2fc4c8c218be0cd9a489906f77d8b9bd9d643dd6
TheElyphant <eodagba@outlook.com>
2023-09-02 00:50:39 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
+#include <stdlib.h>
 #include <stdio.h>
-
+/**
+* main - Entry point of the program
+* @argc: input
+* @argv: input
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
