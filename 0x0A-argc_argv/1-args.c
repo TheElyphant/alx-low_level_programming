191729457322765e346c57fbaf9d04362812610e
TheElyphant <eodagba@outlook.com>
2023-09-02 01:23:29 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* @argc: input
+* @argv: input
+* Return: Alway 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("argc = %d\n", argc);
+	return (0);
+}
