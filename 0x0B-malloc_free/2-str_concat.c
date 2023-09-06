d047fd1fcb4a0d468f85220e8b8163513cfec5d4
TheElyphant <eodagba@outlook.com>
2023-09-06 22:38:32 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* *str_concat* - concatenates two strings.
+* *str_concat - concatenates two strings.
 * @s1: input string
 * @s2: input string
 * Return: 0
