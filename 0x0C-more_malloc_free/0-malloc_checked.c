41e5c0549a600a00b5ced8df34df0e9df4681e29
TheElyphant <eodagba@outlook.com>
2023-09-08 22:10:59 +0100

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -13,19 +13,8 @@ void *malloc_checked(unsigned int b)
 
 	if (ptr == NULL)
 	{
-		fprintf(stderr, "Memory allocation failed\n");
+		printf("Memory allocation failed\n");
 		exit(98);
 	}
 	return (ptr);
-}
-/**
-* main - allocate memory of 1024
-* Return: 0
-*/
-int main(void)
-{
-	char *myArray = (char *)malloc_checked(sizeof(char) * 1024);
-
-	free(myArray);
-	return (0);
 }
