be0a9fa09b1a3305fd2ba5c7086963a88e0eaa1f
TheElyphant <eodagba@outlook.com>
2023-09-13 00:40:42 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MYSTRUCT_H
+#define MYSTRUCT_H
+
+#include <stdio.h>
+/**
+* struct dog - new type
+* @name: a character
+* @age: a float
+* @owner: a character
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* MYSTRUCT_H */
