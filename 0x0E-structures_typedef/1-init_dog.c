3561ceaf934e90a7d8897e1cb10b4a10ff96f1cb
TheElyphant <eodagba@outlook.com>
2023-09-13 20:46:07 +0100

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
 * init_dog - initialize a variable of type struct dog
@@ -10,11 +10,8 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!d)
-	{
-		printf("Error: Provided pointer is NULL. Cannot initialize the dog struct.\n");
-		return;
-	}
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
 /**
 * struct dog - new type
 * @name: a character
 * @age: a float
 * @owner: a character
+* Description: Longer description
 */
 struct dog
 {
@@ -14,7 +14,14 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-#endif /* DOG_H */
+#endif
