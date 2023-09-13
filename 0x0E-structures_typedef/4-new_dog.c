44e1709d6aab619783efab94acdfb5b5881f9a5f
TheElyphant <eodagba@outlook.com>
2023-09-13 23:58:26 +0100

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+* *new_dog - creates a new dog.
+* @name: new name
+* @age: new age
+* @owner: new owner
+* Return: pointer to new dog (Success), NULL otherwise
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+
+	if (!name  || !owner)
+	{
+		return (NULL);
+	}
+
+	newDog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (!newDog)
+	{
+		return (NULL);
+	}
+
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+
+	if (!newDog->name || !newDog->owner)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+	newDog->age = age;
+	return (newDog);
+}
