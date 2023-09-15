f9e288b3fe925b4ff9417066f7539ddb72a32b5b
TheElyphant <eodagba@outlook.com>
2023-09-15 20:59:07 +0100

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (array == NULL)
+	{
+		printf("Array pointer is NULL\n");
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
