0f7b45244682f8d03f5ee953e60e8eb99b12e0bf
TheElyphant <eodagba@outlook.com>
2023-09-15 22:28:58 +0100

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,7 +4,7 @@
 * main - prints the opcodes
 * @argc: argument count
 * @argv: argument array
-* Return: Always 0 (Success) 
+* Return: Always 0 (Success)
 */
 int main(int argc, char *argv[])
 {
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		exit(1);
 	}
 
 	num_bytes = atoi(argv[1]);
@@ -23,15 +23,19 @@ int main(int argc, char *argv[])
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		return (2);
+		exit(2);
 	}
 
 	main_ptr = (unsigned char *)main;
 
 	for (i = 0; i < num_bytes; i++)
 	{
-		printf("%02x", main_ptr[i]);
+		if (i == num_bytes - 1)
+		{
+			printf("%02hhx\n", main_ptr[i]);
+			break;
+		}
+		printf("%2hhx", main_ptr[i]);
 	}
-	printf("\n");
 	return (0);
 }
