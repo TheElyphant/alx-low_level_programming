1f05f96d8a5e362bd9f19bae75c12e110308684c
TheElyphant <eodagba@outlook.com>
2023-09-15 21:30:32 +0100

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* int_index - return index if comparison = true, else -1
+* @array: element
+* @size: number of elements in the array
+* @cmp: pointer to the functions
+* Return: 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+
+	}
+	return (-1);
+}
