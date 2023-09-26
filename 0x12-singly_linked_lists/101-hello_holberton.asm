d170fe1169f276ee293051c17cd741389d7137a6
TheElyphant <eodagba@outlook.com>
2023-09-26 01:57:11 +0100

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
-	hello: db "Hello, Holberton", 0
-	fmt: db "%s", 0
+	hello db "Hello, Holberton", 0
+	format db "%s", 0
 
 section .text
 	global main
@@ -8,8 +8,9 @@ section .text
 
 main:
 	push rbp
-	mov rdi, fmt
-	mov rsi, hello
+	mov rdi, format
+	mov rax, 0
+	lea rsi, [rel hello]
 	call printf
 	pop rbp
 	ret
