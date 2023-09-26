0d7f80fab55d55702430c446310416d4c40634a4
TheElyphant <eodagba@outlook.com>
2023-09-26 01:13:05 +0100

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* before_main - print desired text.
+* Return: 0
+*/
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+* main - main function
+* Return: 0
+*/
+int main(void)
+{
+	before_main();
+	return (0);
+}
