84902c7c9491db3fa849488ea5877fe11bee1653
TheElyphant <eodagba@outlook.com>
2023-09-26 01:24:19 +0100

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,9 @@
 * before_main - print desired text.
 * Return: 0
 */
-void before_main(void)
+void construction(void) __attribute__((constructor));
+
+void construction(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
