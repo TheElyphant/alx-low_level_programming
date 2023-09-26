df98d05200ccb6cbe5537718c9319243d712f7dd
TheElyphant <eodagba@outlook.com>
2023-09-26 01:17:05 +0100

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,13 +8,3 @@ void before_main(void)
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
-/**
-* main - main function
-* Return: 0
-*/
-int main(void)
-{
-	before_main();
-	return (0);
-}
