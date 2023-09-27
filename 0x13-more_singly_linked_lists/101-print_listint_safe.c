5bc994673ace4a3dd344c1590e0868723e6f0477
TheElyphant <eodagba@outlook.com>
2023-09-27 01:53:43 +0100

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,61 @@
 #include "lists.h"
 /**
-* print_listint_safe - prints a listint_t linked list.
-* @head: Pointer to the head of the list.
-* Return: number of nodes.
-*/
-size_t print_listint_safe(const listint_t *head)
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *current = head;
-	size_t node_count = 0;
+	const listint_t *tortoise = head;
+	const listint_t *hare = head;
+	size_t nodes = 0;
 	int is_loop = 0;
 
-	if (head == NULL)
-		return (0);
-
-	while (current != NULL)
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		node_count++;
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 
-		if (current->next >= current)
+		if (tortoise == hare)
 		{
 			is_loop = 1;
 			break;
 		}
-		current = current->next;
 	}
-	if (is_loop)
+	if (!is_loop)
 	{
-		printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-		exit(98);
+		return (nodes);
 	}
-	return (node_count);
+	do {
+		nodes++;
+		tortoise = tortoise->next;
+	}
+	while (tortoise != hare);
+	return (nodes);
+}
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = looped_listint_len(head);
+	int is_loop = (nodes > 0);
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+
+		if (is_loop && nodes == 1)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+		head = head->next;
+		nodes--;
+	}
+	return (looped_listint_len(head));
 }
