176b37da775a5157dbd548e855be8bed54ca99e7
TheElyphant <eodagba@outlook.com>
2023-10-03 23:27:55 +0100

    0-read_textfile.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+/**
+* create_file - function that creates a file.
+* @filename: file to create.
+* @text_content: NULL terminated string.
+* Return: 0
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(file_descriptor, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+	close(file_descriptor);
+	return (1);
+}
